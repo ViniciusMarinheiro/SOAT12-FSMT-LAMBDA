1b7f786971e21a7d022d208fa1630847854f2adb
ViniciusMarinheiro <77128904+ViniciusMarinheiro@users.noreply.github.com>
2025-12-13 22:05:02 +0000

    Fix: Move recursos para West US 2 para fugir do bloqueio de cota

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,42 +17,37 @@ provider "azurerm" {
   features {}
 }
 
-# 1. Recupera o Resource Group (que está em East US 2)
+# 1. Recupera o Resource Group (que está no East US 2, mas tudo bem)
 data "azurerm_resource_group" "rg" {
   name = "rg-fsmt-soat12"
 }
 
 # 2. Storage Account
+# MUDANÇA 1: Região West US 2
+# MUDANÇA 2: Nome novo (v2) para evitar conflito com o anterior
 resource "azurerm_storage_account" "sa_func" {
-  name                     = "stfuncsoat12fsmt"
+  name                     = "stfuncsoat12fsmtv2" 
   resource_group_name      = data.azurerm_resource_group.rg.name
-  
-  # VOLTAMOS PARA A REGIÃO DO GRUPO (East US 2)
-  location                 = data.azurerm_resource_group.rg.location 
-  
+  location                 = "westus2" 
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
 
-# 3. Plano de Serviço (B1 em East US 2)
+# 3. Plano de Serviço (B1 em West US 2)
+# A região West US 2 costuma ter cota liberada para B1
 resource "azurerm_service_plan" "asp" {
   name                = "asp-func-soat12"
   resource_group_name = data.azurerm_resource_group.rg.name
-  
-  # VOLTAMOS PARA A REGIÃO DO GRUPO
-  location            = data.azurerm_resource_group.rg.location
-  
+  location            = "westus2"
   os_type             = "Linux"
-  sku_name            = "B1" # Mantemos B1 pois é o mais seguro para cota
+  sku_name            = "B1" 
 }
 
 # 4. A Function App
 resource "azurerm_linux_function_app" "func_app" {
   name                = "func-auth-soat12"
   resource_group_name = data.azurerm_resource_group.rg.name
-  
-  # VOLTAMOS PARA A REGIÃO DO GRUPO
-  location            = data.azurerm_resource_group.rg.location
+  location            = "westus2"
 
   storage_account_name       = azurerm_storage_account.sa_func.name
   storage_account_access_key = azurerm_storage_account.sa_func.primary_access_key
