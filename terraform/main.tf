369ef7abce3bbcf4edda15f1bb204eae652922ee
ViniciusMarinheiro <77128904+ViniciusMarinheiro@users.noreply.github.com>
2025-12-18 00:03:41 +0000

    Fix: Migra infra para Windows App Service para contornar cota Linux

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,37 +17,38 @@ provider "azurerm" {
   features {}
 }
 
-# 1. Recupera o Resource Group (que está no East US 2)
+# 1. Recupera o Resource Group (East US 2)
 data "azurerm_resource_group" "rg" {
   name = "rg-fsmt-soat12"
 }
 
 # 2. Storage Account
-# MUDANÇA 1: Região North Europe (Irlanda) - Geralmente tem cota para estudantes
-# MUDANÇA 2: Nome v3
+# Mantivemos o nome v3 para garantir unicidade
 resource "azurerm_storage_account" "sa_func" {
-  name                     = "stfuncsoat12fsmtv3" 
+  name                     = "stfuncsoat12fsmtv3"
   resource_group_name      = data.azurerm_resource_group.rg.name
-  location                 = "northeurope" 
+  location                 = data.azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
 
-# 3. Plano de Serviço
-# Tentamos B1 (Basic) na Europa. Se falhar, a última opção será Windows.
+# 3. Plano de Serviço (MUDANÇA: WINDOWS)
+# Mudamos para Windows para usar um "balde" de cota diferente
 resource "azurerm_service_plan" "asp" {
   name                = "asp-func-soat12"
   resource_group_name = data.azurerm_resource_group.rg.name
-  location            = "northeurope"
-  os_type             = "Linux"
-  sku_name            = "B1" 
+  location            = data.azurerm_resource_group.rg.location
+  
+  os_type             = "Windows" # <--- MUDANÇA CRÍTICA
+  sku_name            = "B1"      # Basic Tier
 }
 
-# 4. A Function App
-resource "azurerm_linux_function_app" "func_app" {
+# 4. A Function App (MUDANÇA: WINDOWS FUNCTION)
+# Note que o recurso agora é "azurerm_windows_function_app"
+resource "azurerm_windows_function_app" "func_app" {
   name                = "func-auth-soat12"
   resource_group_name = data.azurerm_resource_group.rg.name
-  location            = "northeurope"
+  location            = data.azurerm_resource_group.rg.location
 
   storage_account_name       = azurerm_storage_account.sa_func.name
   storage_account_access_key = azurerm_storage_account.sa_func.primary_access_key
@@ -55,20 +56,22 @@ resource "azurerm_linux_function_app" "func_app" {
 
   site_config {
     application_stack {
-      node_version = "18"
+      node_version = "~18" # Sintaxe para Node no Windows Function
     }
-    always_on = true 
+    # Always On ligado no plano B1
+    always_on = true
   }
 
   app_settings = {
     "FUNCTIONS_WORKER_RUNTIME" = "node"
+    "WEBSITE_NODE_DEFAULT_VERSION" = "~18"
   }
 }
 
 output "function_app_name" {
-  value = azurerm_linux_function_app.func_app.name
+  value = azurerm_windows_function_app.func_app.name
 }
 
 output "function_app_default_hostname" {
-  value = azurerm_linux_function_app.func_app.default_hostname
+  value = azurerm_windows_function_app.func_app.default_hostname
 }
