c809f1472624daac05a50cdd396503d8b1136d75
ViniciusMarinheiro <77128904+ViniciusMarinheiro@users.noreply.github.com>
2025-12-18 00:28:50 +0000

    Infra: Reverte para Azure Function App (Serverless) após upgrade da conta

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,31 +17,59 @@ provider "azurerm" {
   features {}
 }
 
-# 1. Recupera o Resource Group (Já sabemos que está em East US 2)
+# 1. Recupera o Resource Group existente
 data "azurerm_resource_group" "rg" {
   name = "rg-fsmt-soat12"
 }
 
-# 2. Azure Container Registry (ACR)
-# Vai ser criado na mesma região do RG (East US 2)
-# O SKU "Basic" é barato e não tem o problema de cota das VMs
-resource "azurerm_container_registry" "acr" {
-  name                = "acrsoat12fsmt" # Tudo minúsculo, deve ser único
+# 2. Storage Account (Obrigatório para a Function funcionar)
+resource "azurerm_storage_account" "sa_func" {
+  name                     = "stfuncsoat12fsmt" # Deve ser único globalmente
+  resource_group_name      = data.azurerm_resource_group.rg.name
+  location                 = data.azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+# 3. Plano de Serviço (Consumption / Serverless)
+resource "azurerm_service_plan" "asp" {
+  name                = "asp-func-soat12"
   resource_group_name = data.azurerm_resource_group.rg.name
   location            = data.azurerm_resource_group.rg.location
-  sku                 = "Basic"
-  admin_enabled       = true # Importante: Habilita login para o GitHub Actions
+  os_type             = "Linux"
+  sku_name            = "Y1" # Y1 = Serverless (Paga por execução)
 }
 
-output "acr_login_server" {
-  value = azurerm_container_registry.acr.login_server
+# 4. A Function App (Node.js)
+resource "azurerm_linux_function_app" "func_app" {
+  name                = "func-auth-soat12" # URL: https://func-auth-soat12.azurewebsites.net
+  resource_group_name = data.azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.rg.location
+
+  storage_account_name       = azurerm_storage_account.sa_func.name
+  storage_account_access_key = azurerm_storage_account.sa_func.primary_access_key
+  service_plan_id            = azurerm_service_plan.asp.id
+
+  site_config {
+    application_stack {
+      node_version = "18"
+    }
+    # Cors (opcional, mas bom para evitar erro se chamar do front)
+    cors {
+      allowed_origins = ["*"]
+    }
+  }
+
+  app_settings = {
+    "FUNCTIONS_WORKER_RUNTIME" = "node"
+    # Placeholder: As variáveis reais (DB e JWT) serão injetadas via App Settings no Portal ou CLI
+  }
 }
 
-output "acr_admin_username" {
-  value = azurerm_container_registry.acr.admin_username
+output "function_app_name" {
+  value = azurerm_linux_function_app.func_app.name
 }
 
-output "acr_admin_password" {
-  value = azurerm_container_registry.acr.admin_password
-  sensitive = true
+output "function_app_default_hostname" {
+  value = azurerm_linux_function_app.func_app.default_hostname
 }
